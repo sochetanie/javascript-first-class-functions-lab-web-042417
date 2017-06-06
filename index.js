84e0f114f6a313bcc1993860d7b163a47a823182
sochetanie <allagorik2901@gmail.com>
2017-06-06 15:11:33 -0400

    done

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ var tripler = createMultiplier(3)
 function multiplier(num1, num2) {
     return num1 * num2
 }
-
 var doublerWithBind = multiplier.bind(null, 2)
 var triplerWithBind = multiplier.bind(null, 3)
+
