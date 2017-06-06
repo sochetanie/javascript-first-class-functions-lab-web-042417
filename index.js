c865adf7a25fde691d7f126ad1a14fa2e0325525
sochetanie <allagorik2901@gmail.com>
2017-06-06 15:10:00 -0400

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+function countdown(callback) {
+    window.setTimeout(callback, 2000)
+}
+
+
+function createMultiplier(multiplierValue) {
+    return function (num) {
+        return num * multiplierValue
+    }
+}
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+
+function multiplier(num1, num2) {
+    return num1 * num2
+}
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
